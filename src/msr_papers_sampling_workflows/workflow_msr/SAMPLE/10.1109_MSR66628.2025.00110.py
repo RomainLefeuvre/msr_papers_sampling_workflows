5264e30ae406d113ef180c89ee39a9863e79b5ac
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2025-10-08 15:00:43 +0200

    add workflow : 10.1109_MSR66628.2025.00110.py

---
+++ src/msr_papers_sampling_workflows/workflow_msr/SAMPLE/10.1109_MSR66628.2025.00110.py
@@ -0,0 +1,25 @@
+# * Use Islam et al PD dataset
+# * random partition 80-20, 5 times 
+
+from pathlib import Path
+
+from sampling_mining_workflows_dsl.element.writer.CsvWriter import CsvWriter
+from sampling_mining_workflows_dsl.element.loader import JsonLoader
+from sampling_mining_workflows_dsl.WorkflowBuilder import WorkflowBuilder
+from sampling_mining_workflows_dsl.operator.OperatorBuilder import OperatorBuilder
+from sampling_mining_workflows_dsl.metadata.Metadata import Metadata
+
+def main():
+    input_path = Path("input.json")
+    url = Metadata.of_string("url")
+
+    #Execute 5 time the workflow
+    for i in range(5):
+        WorkflowBuilder()\
+            .input(JsonLoader(input_path, url))\
+            .grouping_operator(
+                OperatorBuilder().random_selection_operator(80, 5)\
+                                 .set_output_set_id("random_output_set"),
+                OperatorBuilder().difference_with_operator("random_output_set")
+            )\
+            .output(CsvWriter(f"result{i}.csv"))\
