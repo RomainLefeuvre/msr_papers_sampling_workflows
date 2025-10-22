668e026a18a54f4facafd9a7765623235ce7bf6a
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2025-10-22 23:06:00 +0200

    Update running example

--- src/msr_papers_sampling_workflows/paper_methodo_dblp.py
+++ src/msr_papers_sampling_workflows/paper_methodo_dblp.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
+from sampling_mining_workflows_dsl.analysis.CoverageTest import CoverageTest
 from sampling_mining_workflows_dsl.analysis.HistWorkflowAnalysis import (
     HistWorkflowAnalysis,
 )
+from sampling_mining_workflows_dsl.analysis.ChiSquareAnalysis import ChiSquareAnalysis
 from sampling_mining_workflows_dsl.element.loader.CsvLoader import CsvLoader
 from sampling_mining_workflows_dsl.element.writer.CsvWriter import CsvWriter
 from sampling_mining_workflows_dsl.metadata.Metadata import Metadata
@@ -35,24 +37,32 @@ def main():
         .add_metadata(CsvLoader(IEEE_path, doi, iee_keyword_list))
         .random_selection_operator(cardinality=65, seed=42)
         .output(CsvWriter("data/sample.csv"))
-    )
+    ).execute_workflow()
+     
     # Workflow Execution
-    workflow.execute_workflow()
-    # Workflow Analysis
-    HistWorkflowAnalysis(year,100,category=True,sort=False).analyze(workflow)
-    HistWorkflowAnalysis(iee_keyword_list,top_x=50,category=True,sort=True).analyze(workflow)
-    CoverageTest(iee_keyword_list,workflow.get_operator_by_position(1).get_output(),
-                                   workflow.get_workflow_output()).compute_coverage(50)
+#     workflow.execute_workflow()
+#     # Workflow Analysis
+#    # HistWorkflowAnalysis(year,100,category=True,sort=False).analyze(workflow)
+#     HistWorkflowAnalysis(iee_keyword_list,top_x=50,category=True,sort=True).analyze(workflow)
+
+#     set_1 = workflow.get_operator_by_position(2).get_input()
+#     set_2 = workflow.get_operator_by_position(2).get_output()
+
+#     ChiSquareAnalysis(iee_keyword_list).analyze(set_1, set_2)
+    print(workflow)
+    
+    # CoverageTest(iee_keyword_list,set_1,set_2)\
+    #         .compute_coverage(50)
 
  
 
 
 if __name__ == "__main__":
     main()
-   # chi2analysis = ChiSquareAnalysis(iee_keyword_list)
-    # set_1 = workflow.get_operator_by_position(2).get_input()
-    # set_2 = workflow.get_operator_by_position(2).get_output()
-    # chi2analysis.analyze(set_1, set_2)
+    # chi2analysis = ChiSquareAnalysis(iee_keyword_list)
+    #  set_1 = workflow.get_operator_by_position(2).get_input()
+    #  set_2 = workflow.get_operator_by_position(2).get_output()
+      #chi2analysis.analyze(set_1, set_2)
     # WorkflowVisualizer(workflow).generate_graph()
     # print(workflow)
     # DistributionWorkflowAnalysis(year).analyze(workflow)
--- src/msr_papers_sampling_workflows/running_exemple.py
+++ src/msr_papers_sampling_workflows/running_exemple.py
@@ -1,30 +1,50 @@
+from pathlib import Path
+from sampling_mining_workflows_dsl.operator.OperatorBuilder import OperatorBuilder
 from sampling_mining_workflows_dsl.WorkflowBuilder import WorkflowBuilder
+
 from sampling_mining_workflows_dsl.element.Loader import Loader
 from datetime import datetime
+from sampling_mining_workflows_dsl.metadata.Metadata import Metadata
+from sampling_mining_workflows_dsl.element.writer.JsonWriter import JsonWriter
+from sampling_mining_workflows_dsl.element.writer.CsvWriter import CsvWriter
 
+from sampling_mining_workflows_dsl.element.loader.JsonLoader import JsonLoader
+from sampling_mining_workflows_dsl.Workflow import Workflow
+
+from sampling_mining_workflows_dsl.analysis.HistWorkflowAnalysis import (
+    HistWorkflowAnalysis,
+)
+from sampling_mining_workflows_dsl.exec_visualizer.WorkflowVisualizer import WorkflowVisualizer
 
 # ---- Metadata ----
+url = Metadata.of_string("url")
 latest_commit_date = Metadata.of_date("latest_commit_date")
-author_nb = Metadata.of_integer("author_Nb")
+number_of_commits = Metadata.of_integer("number_of_commits")
+number_of_commiters = Metadata.of_integer("number_of_commiters")
 # ---- Workflow ----
-(
-    WorkflowBuilder()
-    .input(Loader("2024-05-16-history-hosting"))
-    .filter_operator("latest_commit_date > datetime(2023, 1, 1)")
+
+workflow = WorkflowBuilder()\
+    .input(JsonLoader(Path("test.json"),url,latest_commit_date,number_of_commits,number_of_commiters))\
+    .filter_operator(latest_commit_date.is_after(datetime(2023, 1, 1)))\
     .grouping_operator(
         # First stratum: projects with less than 5 authors
         (
-            WorkflowBuilder()
-            .filter_operator("author_nb < 5")
+            OperatorBuilder(Workflow())
+            .filter_operator("number_of_commiters < 5")
             .random_selection_operator(10000)
         ),
         # Second stratum: projects with 5 or more authors
         (
-            WorkflowBuilder()
-            .filter_operator("author_nb >=5")
+            OperatorBuilder(Workflow())
+            .filter_operator("number_of_commiters >=5")
             .random_selection_operator(10000)
         ),
-    )
-    .union_operator()  # Merge the two samples
-    .execute()
-)
+    )\
+    .union_operator()\
+    .output(JsonWriter("sampled_repos.json"))\
+    .execute_workflow()\
+    .print()
+    
+
+HistWorkflowAnalysis(metadata=number_of_commiters,top_x=-1,category=False,sort=True,output_path="./out").analyze(workflow)
+WorkflowVisualizer(workflow,"./out" ).generate_graph()
