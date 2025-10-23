3bb62f987867216093bfb143708e75e9969493c3
Maïwenn Le Goasteller <maiwenn.le-goasteller@irisa.fr>
2025-10-23 15:27:43 +0200

    Add workflow for 10.1145_3643991.3644923.

---
+++ src/msr_papers_sampling_workflows/workflow_msr/SAMPLE/10.1145_3643991.3644923.py
@@ -0,0 +1,25 @@
+# Data Augmentation for Supervised Code Translation Learning.
+# * Filter for Java and C# projects
+# * Remove projects part of the other dataset
+
+from pathlib import Path
+
+from sampling_mining_workflows_dsl.element.writer.CsvWriter import CsvWriter
+from sampling_mining_workflows_dsl.element import Loader
+from sampling_mining_workflows_dsl.operator.set_algebra.external_set_operator import DifferenceOperator
+from sampling_mining_workflows_dsl.WorkflowBuilder import WorkflowBuilder
+from sampling_mining_workflows_dsl.operator.OperatorBuilder import OperatorBuilder
+from sampling_mining_workflows_dsl.metadata.Metadata import Metadata
+
+def main():
+    input_path = Path("input.json")
+    url = Metadata.of_string("url")
+    language = Metadata.of_string("language")
+
+    #Execute 5 time the workflow
+    for i in range(5):
+        WorkflowBuilder()\
+            .input(Loader("PGA dataset", url, language))\
+            .filter_operator("language == 'Java' or language == 'C#'")\
+            .difference_with_external_set_operator(Loader("java-c# dataset", url))\
+            .output(CsvWriter(f"result{i}.csv"))
