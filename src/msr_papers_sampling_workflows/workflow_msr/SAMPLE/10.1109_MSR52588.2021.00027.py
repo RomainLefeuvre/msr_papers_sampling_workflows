7e22075a413b7b49b2da41c14bba9ce1dfcc1d75
mimipepin <74369528+mimipepin@users.noreply.github.com>
2025-10-23 15:40:26 +0200

    Add production_related_files_nb filter to workflow

--- src/msr_papers_sampling_workflows/workflow_msr/SAMPLE/10.1109_MSR52588.2021.00027.py
+++ src/msr_papers_sampling_workflows/workflow_msr/SAMPLE/10.1109_MSR52588.2021.00027.py
@@ -14,11 +14,13 @@ from sampling_mining_workflows_dsl.WorkflowBuilder import WorkflowBuilder
 # filter clonable project
 # add percentage_log_method
 # filter project with at least 4% of percentage_log_method
+# filter project with more than 100
 
 url = Metadata.of_string("id")
 language = Metadata.of_string("language")
 is_clonable = Metadata.of_boolean("isClonable")
 percentage_log_method = Metadata.of_double("percentageLogMethod")
+production_related_files_nb = Metadata.of_long("ProdRelatedFiles")
 
 
 def apache_foundation_workflow():
@@ -31,6 +33,7 @@ def apache_foundation_workflow():
                 language,
                 is_clonable,
                 percentage_log_method,
+                production_related_files_nb,
             )
         )
         # Filter Java projects
@@ -43,6 +46,9 @@ def apache_foundation_workflow():
         .add_metadata(percentage_log_method)
         # Filter project with at least 4% logging methods
         .filter_operator("percentageLogMethod >= 0.04")
+        # filter project with more than 100
+        .add_metadata(production_related_files_nb)
+        .filter_operator("production_related_files_nb > 100")
         .output(CsvWriter("apache_foundation_filtered.csv"))
     )
 
